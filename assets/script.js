13e6f90c923d17477275aede2a6e2144cd33a73c
Idriss <44480885+m-idriss@users.noreply.github.com>
2025-08-21 15:20:08 +0200

    script.js: delete deviceorientation mobile event

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,1 @@
-window.addEventListener('deviceorientation', (event) => {
-  const gamma = event.gamma || 0; 
-  const beta = event.beta || 0;
 
-  const maxShift = 15;
-  const xShift = Math.max(Math.min(gamma, 30), -30) / 30 * maxShift;
-  const yShift = Math.max(Math.min(beta, 30), -30) / 30 * maxShift;
-
-  document.body.style.backgroundPosition = `calc(50% + ${xShift}px) calc(50% + ${yShift}px)`;
-});
