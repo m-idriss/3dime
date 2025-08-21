3a9fd6dc0644f8497317aaef78254b10656fd969
Idriss <44480885+m-idriss@users.noreply.github.com>
2025-08-22 00:45:05 +0200

    add github public badge number of repositories

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,22 @@
-<script>
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('assets/sw.js')
     .then(() => console.log('SW registered'))
     .catch(err => console.log('SW failed', err));
 }
-</script>
+
+document.addEventListener("DOMContentLoaded", () => {
+  const badge = document.getElementById("badge-github");
+
+  fetch("https://api.github.com/users/m-idriss")
+    .then(response => response.json())
+    .then(data => {
+      const repoCount = data.public_repos;
+
+      badge.textContent = repoCount;
+
+      badge.style.display = repoCount > 0 ? "flex" : "none";
+    })
+    .catch(error => {
+      console.error("Error fetching GitHub data:", error);
+    });
+});
