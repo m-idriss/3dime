30f3acde5c7bf2453abd045a2344406c6a601507
Idriss <44480885+m-idriss@users.noreply.github.com>
2025-08-22 00:16:32 +0200

    try mobile view

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,7 @@
-
+<script>
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('assets/sw.js')
+    .then(() => console.log('SW registered'))
+    .catch(err => console.log('SW failed', err));
+}
+</script>
